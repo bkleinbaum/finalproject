cc7b992c368e28540c117217bdc69ea0ecfa6899
bkleinbaum <ben.kleinbaum@gmail.com>
2015-04-28 18:41:20 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -60,7 +60,7 @@ $.getJSON(link+query)
     console.log(councilGeoJson);
     console.log(search)
 
-    var tagged = turf.tag(search, councilFinal
+    var tagged = turf.tag(search, councilFinal,
                       'coundist', 'councilDistrict');
     console.log(tagged)
   });     
